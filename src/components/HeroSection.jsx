7aba1e66ddee7b7271e801954ed2269d9d6ffa67
XY2329 <seexy1127@gmail.com>
2024-06-05 10:42:49 +0800

    resume3

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -41,7 +41,7 @@ const HeroSection = () => {
               Slide down to know more about me!
             </p>
             <a
-              href="https://drive.google.com/file/d/18TfNdaJVSNvp0U-9fRQTm0LbQRIJwAHG/view?usp=sharing"
+              href="https://drive.google.com/file/d/1MxELiWopw4mmbzID1wAeVqhC3_TwomCb/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="px-6 inline-block py-3 rounded-full border-2 border-pink-500 hover:bg-pink-400"
